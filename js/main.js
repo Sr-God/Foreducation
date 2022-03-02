1551000af3976ee2d59130e936e466499a2af9f3
Sr-God <srgod@vk.com>
2022-03-02 23:47:53 +0200

    Authorization

--- js/main.js
+++ js/main.js
@@ -5,3 +5,23 @@
             body.classList.toggle("dark");
             themeSwitch.classList.toggle('active');
     });
+
+    /* LOGIN */
+
+//     let loginIn = document.querySelector('#username');
+//     let passIn = document.querySelector('#password');
+//     let userReq = document.querySelector('.userReq');
+//     let passReq = document.querySelector('.passReq');
+//     let eyeBtn = document.querySelector('#eye')
+//     // CHECK USER VALID
+//     loginIn.addEventListener('input', function (){
+//         userReq.className = loginIn.validity.valid ? 'hide' : '';
+//     })
+//     // CHECK PASSWORD VALID
+//     passIn.addEventListener('input', function (){
+//         passReq.className = passIn.validity.valid ? 'hide' : '';
+//     })
+//     // EYE
+//     eyeBtn.addEventListener('click', function (){
+//         passIn.type = eyeBtn.checked ? 'text' : 'password';
+//     })

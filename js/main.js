fe68f7cd03a4182a3b575cfafce769559a1f337b
Sr-God <srgod@vk.com>
2022-03-03 12:26:55 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 'use strict';
+    /* Theme Switch */
     let body = document.querySelector('body');
     let themeSwitch = document.querySelector('#themeSwitch');    
     themeSwitch.addEventListener('click', function(){
@@ -6,22 +7,4 @@
             themeSwitch.classList.toggle('active');
     });
 
-    /* LOGIN */
 
-//     let loginIn = document.querySelector('#username');
-//     let passIn = document.querySelector('#password');
-//     let userReq = document.querySelector('.userReq');
-//     let passReq = document.querySelector('.passReq');
-//     let eyeBtn = document.querySelector('#eye')
-//     // CHECK USER VALID
-//     loginIn.addEventListener('input', function (){
-//         userReq.className = loginIn.validity.valid ? 'hide' : '';
-//     })
-//     // CHECK PASSWORD VALID
-//     passIn.addEventListener('input', function (){
-//         passReq.className = passIn.validity.valid ? 'hide' : '';
-//     })
-//     // EYE
-//     eyeBtn.addEventListener('click', function (){
-//         passIn.type = eyeBtn.checked ? 'text' : 'password';
-//     })

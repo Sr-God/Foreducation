7b1f4a096ab6ed94b77b91d45f0d6cf38f29e9d3
limon4ik-28 <92482448+limon4ik-28@users.noreply.github.com>
2022-05-25 11:23:17 +0200

    mobile

--- js/burger.js
+++ js/burger.js
@@ -5,9 +5,7 @@ menuBtn.addEventListener('click', function(){
     menuBtn.classList.toggle('open');
     if(nav.style.display == 'flex'){
         nav.style.display = 'none';
-        console.log(1);
     }else{
         nav.style.display = 'flex';
-        console.log(2);
     }
 })
